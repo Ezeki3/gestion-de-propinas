3f8eac614cd2ee68129d54698f0410325e6ba6b0
Ezequiel Lino <sonisombra@gmail.com>
2023-07-16 21:42:49 -0600

    validacion de campos vacios

--- js/app.js
+++ js/app.js
@@ -9,5 +9,15 @@ const btnGuardarCliente = document.querySelector('#guardar-cliente');
 btnGuardarCliente.addEventListener('click',  guardarCliente);
 
 function guardarCliente(){
-  console.log('esta funcionando');
+  const mesa = document.querySelector('#mesa').value;
+  const hora = document.querySelector('#hora').value;
+
+  // revisamos si hay campos vacios
+  const camposVacios = [mesa, hora].some( campo => campo === '');
+
+  if(camposVacios){
+    console.log('al menos un campo esta vacio');
+  } else {
+    console.log('todos los campos estan llenos');
+  }
 }
