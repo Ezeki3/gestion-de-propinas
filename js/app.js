1c52c759ea596f44e5f129db0c64d43cac5d0b41
Ezequiel Lino <sonisombra@gmail.com>
2024-01-02 11:48:45 -0600

    limpiar el resumen al actualizar el pedido

--- js/app.js
+++ js/app.js
@@ -142,6 +142,8 @@ function agregarPlatillo(producto){
     const resultado = pedido.filter( articulo => articulo.id !== producto.id );
     cliente.pedido = [...resultado];
   }
+  // Limpiar el codigo HTML previo
+  limpiarHTML();
 
   // Mostrar el resumen
   actualizarResumen();
@@ -152,7 +154,7 @@ function actualizarResumen() {
   const contenido = document.querySelector('#resumen .contenido');
 
   const resumen = document.createElement('DIV');
-  resumen.classList.add('col-md-6');
+  resumen.classList.add('col-md-6', 'card', 'py-5', 'px-3', 'shadow');
 
   // Informacion de la mesa
   const mesa = document.createElement('P');
@@ -176,8 +178,26 @@ function actualizarResumen() {
   mesa.appendChild(mesaSpan);
   hora.appendChild(horaSpan);
 
+  // Titulo de la seccion
+  const heading = document.createElement('H3');
+  heading.textContent = "Platillos consumidos";
+  heading.classList.add('my-4','text-center');
+
+  // Iterar sobre el array de pedidos
+
 
   // Agregar al contenido
-  contenido.appendChild(mesa);
-  contenido.appendChild(hora);
+  resumen.appendChild(mesa);
+  resumen.appendChild(hora);
+  resumen.appendChild(heading);
+
+  contenido.appendChild(resumen);
+}
+
+function limpiarHTML() {
+  const contenido = document.querySelector('#resumen .contenido');
+
+  while ( contenido.firstChild ) {
+    contenido.removeChild(contenido.firstChild);
+  }
 }
