5d05e73d42e990f6cd947e00469a7c4116b76bf7
Ezequiel Lino <sonisombra@gmail.com>
2024-01-03 12:05:37 -0600

    mesnajes condicionales segun el pedido del usuario

--- js/app.js
+++ js/app.js
@@ -145,8 +145,12 @@ function agregarPlatillo(producto){
   // Limpiar el codigo HTML previo
   limpiarHTML();
 
-  // Mostrar el resumen
-  actualizarResumen();
+  if( cliente.pedido.length ){
+    // Mostrar el resumen
+    actualizarResumen();
+  } else {
+    mensajePedidoVacio();
+  }
 
 }
 
@@ -282,6 +286,26 @@ function eliminarProducto(id) {
   // limpiar el codigo html previo
   limpiarHTML();
 
-  // mostrar el resumen
-  actualizarResumen();
+  if( cliente.pedido.length ){
+    // Mostrar el resumen
+    actualizarResumen();
+  } else {
+    mensajePedidoVacio();
+  }
+
+  // El producto se elimino por lo tanto regresamos la cantidad a 0 en el formulario
+  const productoEliminado = `#producto-${id}`;
+  const inputEliminado = document.querySelector(productoEliminado);
+  inputEliminado.value = 0;
+  
+}
+
+function mensajePedidoVacio() {
+  const contenido = document.querySelector('#resumen .contenido');
+
+  const texto = document.createElement('P');
+  texto.classList.add('text-center');
+  texto.textContent = 'Añade los elementos del pedido';
+
+  contenido.appendChild(texto);
 }
